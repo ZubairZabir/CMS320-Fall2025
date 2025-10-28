30cec5d8df6e873f53cdb69a548c5f9a15195a58
Allyson <ahussey@rollins.edu>
2025-10-27 21:50:54 -0400

    Score, GameOver, Play button, Player
    Changes to Game Manager.
    
    - game pauses before it starts and after it ends.
    - the score increases as the it passes various pipes
    -the game over text pops up when needed

--- RollyFox/Assets/Scripts/Player.cs
+++ RollyFox/Assets/Scripts/Player.cs
@@ -22,6 +22,15 @@ public class Player : MonoBehaviour
         InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
     }
 
+    private void OnEnable()
+    {
+        Vector3 position = transform.position;
+        position.y = 0f;
+        transform.position = position;
+        direction = Vector3.zero; 
+
+    }
+
     private void Update(){
         if (Input.GetKeyDown(KeyCode.Space) || (Input.GetMouseButtonDown(0))){
             direction = Vector3.up * strength;
@@ -49,7 +58,7 @@ public class Player : MonoBehaviour
         spriteRenderer.sprite = sprites[spriteIndex];
     }
 
-    private void onTriggerEnter2D(Collider2D other) 
+    private void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.gameObject.tag == "Obstacle") {
             FindObjectOfType<GameManager>().GameOver();
