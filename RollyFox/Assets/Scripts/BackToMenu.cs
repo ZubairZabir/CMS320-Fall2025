e8cf6c10770e3cccf2028ee0bbfda07833fff14c
Zubair Zabir <157433578+ZubairZabir@users.noreply.github.com>
2025-11-20 08:39:03 -0500

    Added BackToMenu Button for All Scenes

---
+++ RollyFox/Assets/Scripts/BackToMenu.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        // Make sure the scene name matches exactly your Main Menu scene
+        SceneManager.LoadScene("MainMenu");
+    }
+}
